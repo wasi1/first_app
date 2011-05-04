2869269d991844c7c57216cbf39986538c8c2377
CJR <cjr@cjr-Satellite-L300.(none)>
2011-05-04 16:52:53 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '06894dc7a3b82b53e2cfeb39104c1edeeea6e5b79866043ec44358091bbd8cf8f68cd3c163a5c69efefc533a8675e77ad463eb34e7681b657397703b0cc5cbf6'
